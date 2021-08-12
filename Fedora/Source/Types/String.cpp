7809b714e865711b99187d13c93bce619049c029
L3odr0id <leon.leonc@yandex.ru>
2021-08-12 18:05:06 +0300

    Easter egg

--- Fedora/Source/Types/String.cpp
+++ Fedora/Source/Types/String.cpp
@@ -4,7 +4,12 @@ namespace fedora {
     namespace types {
         // TODO убрать отсылку 
         std::wstring String::eval() {
-            return L'\"' + this->value + L'\"' + L" Как с деньгами обстоят вопросы?";
+            return L'\"' + this->value + L'\"';
+        }
+
+        std::wstring String::getImportantQuestion() {
+            // Строка "как с деньгами обстоят вопросы" представляет ценность для этого проекта
+            return L"Как с деньгами обстоят вопросы?";
         };
     }
 }
