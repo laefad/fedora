a3a93619523ef1ab522aa06c38fd0d0e9adc9106
laefad <FreeFlightA@yandex.ru>
2021-09-23 01:38:15 +0300

    Remove isChar from Token

--- Fedora/Headers/Parser/Token.h
+++ Fedora/Headers/Parser/Token.h
@@ -40,16 +40,6 @@ namespace fedora {
             /// @param type новый тип токена
             void setType(TokenType type);
 
-            /// TODO некорректно для wchar
-            /// Cимвол может состоять из нескольки wchar, в случае, если sizeof(wchar) != 4
-            /// Если токен состоит из 1 символа, то возвращает true
-            bool isChar() const;
-
-            /// TODO некорректно для wchar
-            /// Cимвол может состоять из нескольки wchar, в случае, если sizeof(wchar) != 4
-            /// Если токен состоит из 1 символа и этот символ равен wchar, то возвращает true
-            bool isChar(char8_t wchr) const;
-
             friend bool operator==(Token const &lhs, Token const &rhs);
         };
     }
